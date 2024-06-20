23482a13b168b5b666b111d931afd55f51ceefd3
Daria Senko <dariaxdee1@o2.pl>
2024-06-20 20:08:30 +0200

    Przelicznik walut

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const currency = document.querySelector('#currency');
+const form = document.querySelector('#form');
+const result = document.querySelector('#result');
+const errorLabel = document.querySelector('#error');
 
-const getCurrency = () => {fetch('https://api.nbp.pl/?ref=public-apis')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
- } });
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  errorLabel.textContent = '';
+  const amount = event.target.amount.value;
+  const currency = event.target.currency.value;
+  fetch(`https://api.nbp.pl/api/exchangerates/rates/A/${currency}`)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject();
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const rate = data?.rates?.[0]?.mid;
+      if (!rate) {
+        errorLabel.textContent = 'błąd';
+        return;
+      }
+      result.textContent = (amount * rate).toFixed(2);
+    })
+    .catch(() => {
+      errorLabel.textContent = 'błąd';
+    });
+});
